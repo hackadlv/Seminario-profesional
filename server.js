47fe6a7b1a3ee0e004592d664020388bf0ab184a
hackadlv <53877687+hackadlv@users.noreply.github.com>
2020-10-01 14:36:37 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 const express = required('expressp')
+
+const router = express.Router()
+
+var app = express()
+app.use ( router )
+
+app.listen( 5000 )
+console.log( 'La aplicacion esta escuchando en http://localhost:5000' )
